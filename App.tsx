a1ddafe7558079bda5c27fecb7f40cd6c2fbd637
Your Name <you@example.com>
2026-02-04 16:40:59 +0200

    new10

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import Header from './Header';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import WhyUs from './components/WhyUs';
